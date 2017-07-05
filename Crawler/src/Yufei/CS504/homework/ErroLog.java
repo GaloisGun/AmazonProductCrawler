8fae799c0dc340d3155003c3c3e0fb2036d4bca5
GaloisGun <ywu103@hawk.iit.edu>
2017-07-04 20:36:57 -0700

    Add log method

--- Crawler/src/Yufei/CS504/homework/ErroLog.java
+++ Crawler/src/Yufei/CS504/homework/ErroLog.java
@@ -1,5 +1,48 @@
 package Yufei.CS504.homework;
 
+import java.io.*;
+
 
 public class ErroLog {
+
+    public static void LogInfo(String logFilePath) {
+        try {
+            File file = new File(logFilePath);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter logWriter = new BufferedWriter(fileWriter);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            errInfo(e);
+        }
+    }
+
+    //log exception
+    public static String errInfo(Exception e) {
+        StringWriter sw = null;
+        PrintWriter pw = null;
+        try {
+            sw = new StringWriter();
+            pw = new PrintWriter(sw);
+            //output the error info into printWriter
+            e.printStackTrace(pw);
+            pw.flush();
+            sw.flush();
+        } finally {
+            if (sw != null) {
+                try {
+                    sw.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if (pw != null) {
+                pw.close();
+            }
+        }
+        return sw.toString();
+    }
 }
